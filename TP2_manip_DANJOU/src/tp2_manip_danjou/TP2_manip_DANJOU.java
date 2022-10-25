da960b24a9ef7ae445ef98bcf13e2882f44de423
matthieu danjou <matthieu.danjou@epfedu.fr>
2022-10-25 12:15:51 +0200

    FIN exercice 3

--- TP2_manip_DANJOU/src/tp2_manip_danjou/TP2_manip_DANJOU.java
+++ TP2_manip_DANJOU/src/tp2_manip_danjou/TP2_manip_DANJOU.java
@@ -15,29 +15,26 @@ public class TP2_manip_DANJOU {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    Tartiflette assiette1=new Tartiflette(500);
-    Tartiflette assiette2=new Tartiflette(600);
+        Tartiflette assiette1 = new Tartiflette(500);
+        Tartiflette assiette2 = new Tartiflette(600);
         assiette2.nbCalories = 100;
-        Tartiflette assiette3=assiette2;
-    // deux tartiflette sont crées;
-    System.out.println("nb decalories de Assiette1:"+ assiette1.nbCalories);
-    System.out.println("nb de calories de Assiette2:"+ assiette2.nbCalories);
-    System.out.println("nb de calories de Assiette3 :"+ assiette3.nbCalories) ;
-      assiette2 = assiette1;
-      assiette1 = assiette3;
-    // Aucune des deux propositions n'est juste   
-      Moussaka[]tableau=new Moussaka[10];
-      for (int i=0;i<tableau.length;i++){
-      tableau[i] = new Moussaka(100*(1+i));
-      if (i==tableau.length-1){
-          System.out.println(tableau[i]);
-      }
-      else{
-          System.out.println(tableau[i]);
-          
-      }
-      }
-      }
-    }
-    
+        Tartiflette assiette3 = assiette2;
+        // une seule tartiflette est  crée;
+        System.out.println("nb decalories de Assiette1:"+ assiette1.nbCalories);
+        System.out.println("nb de calories de Assiette2:" + assiette2.nbCalories);
+        System.out.println("nb de calories de Assiette3 :" + assiette3.nbCalories);
+        assiette2 = assiette1;
+        assiette1 = assiette3;
+        // Aucune des deux propositions n'est juste   
+        Moussaka[] tableau = new Moussaka[10];
+        for (int i = 0; i < tableau.length; i++) {
+            tableau[i] = new Moussaka(100 * (1 + i));
+            if (i == tableau.length - 1) {
+                System.out.println(tableau[i]);
+            } else {
+                System.out.println(tableau[i]);
 
+            }
+        }
+    }
+}
