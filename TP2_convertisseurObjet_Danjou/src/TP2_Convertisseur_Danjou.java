99f84ee0a08495e711f63c65caa4cd428950b5e7
matthieu danjou <matthieu.danjou@epfedu.fr>
2022-10-25 11:50:20 +0200

    FIN exercice 2

--- TP2_convertisseurObjet_Danjou/src/TP2_Convertisseur_Danjou.java
+++ TP2_convertisseurObjet_Danjou/src/TP2_Convertisseur_Danjou.java
@@ -14,22 +14,29 @@ public class TP2_Convertisseur_Danjou {
     /**
      * @param args the command line arguments
      */
-
     public static void main(String[] args) {
 
         Convertisseur temperature = new Convertisseur();
 
-        double result = temperature.CelciusVersKelvin(45);
+        double result = temperature.CelciusVersKelvin(40);
 
         System.out.println(result);
 
-        double result1 = temperature.KelvinVersCelcius(100);
+        double result1 = temperature.KelvinVersCelcius(160);
 
         System.out.println(result1);
 
-        double result2 = temperature.KelvinVersFarenheit(-12);
-
+        double result2 = temperature.CelciusVersFarenheit(120);
         System.out.println(result2);
+
+        double result3 = temperature.FarenheitVersCelcius(-15);
+        System.out.println(result3);
+
+        double result4 = temperature.KelvinVersFarenheit(50);
+        System.out.println(result4);
+
+        double result5 = temperature.FarenheitVerskelvin(110);
+        System.out.println(result5);
 
         System.out.println(temperature);
 
@@ -38,54 +45,37 @@ public class TP2_Convertisseur_Danjou {
         Scanner sc;
         sc = new Scanner(System.in);
 
-        int c;
-
         System.out.println("Bonjour, veuillez saisir une valeur");
-        temp = sc.nextDouble();
-        System.out.println("entrez la converstion que vous voulez faire? \n I)CelciusVersKelvin II) KelvinVersCecius III) FarenheitVersCelcius IV)CelciusVersFrenheit V) KelvinVersFarenheit VI) FarenheitVerskelvin  ");
-        c = sc.nextInt();
-        switch (c) {
-            case 1:
-                System.out.println("La température est " + temp + "K");
-                break;
-            case 2:
-                temp = KelvinVersCelcius(temp);
-                System.out.println("La température est " + temp + "C");
-                break;
-            case 3:
-                temp = CelciusVersFarenheit(temp);
-                System.out.println("La température est " + temp + "F");
-                break;
-            case 4:
-                temp = FarenheitVersCelcius(temp);
-                System.out.println("La température est " + temp + "C");
-                break;
-            case 5:
-                temp = KelvinVersFarenheit(temp);
-                System.out.println("La température est " + temp + "F");
-                break;
-            case 6:
-                System.out.println("La température est " + temp + "K");
-                break;
-            default:
+        int a = sc.nextInt();
+        System.out.println("entrez la converstion que vous voulez faire? \n 1)Celcius 2) Kelvin 3) Farenheit");
+        int b = sc.nextInt();
+        System.out.println("entrez la converstion que vous voulez faire? \n 1)Celcius 2) Kelvin 3) Farenheit");
+        int c = sc.nextInt();
+
+        if (a == 1 && b == 2) {
+            double result12 = temperature.CelciusVersKelvin(c);
+            System.out.println(result12);
+        }
+        if (a == 1 && b == 3) {
+            double result13 = temperature.CelciusVersFarenheit(c);
+            System.out.println(result13);
+        }
+        if (a == 2 && b == 1) {
+            double result21 = temperature.KelvinVersCelcius(c);
+            System.out.println(result21);
+        }
+        if (a == 2 && b == 3) {
+            double result23 = temperature.KelvinVersFarenheit(c);
+            System.out.println(result23);
+        }
+        if (a == 3 && b == 1) {
+            double result31 = temperature.FarenheitVersCelcius(c);
+            System.out.println(result31);
+        }
+        if (a == 3 && b == 2) {
+            double result32 = temperature.FarenheitVerskelvin(c);
+            System.out.println(result32);
 
         }
-
     }
-
-    private static double KelvinVersFarenheit(double temp) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    private static double FarenheitVersCelcius(double temp) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    private static double CelciusVersFarenheit(double temp) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    private static double KelvinVersCelcius(double temp) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-}
+}    
