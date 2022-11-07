3a23f56f7dfb1f4f90fbbe134310faa1cf1a0076
matthieu danjou <matthieu.danjou@epfedu.fr>
2022-11-07 11:16:18 +0100

    creation carte identita utilisateur

--- Mapremiereinterfacegraphique/src/mapremiereinterfacegraphique/Mapremiereinterfacegraphique.java
+++ Mapremiereinterfacegraphique/src/mapremiereinterfacegraphique/Mapremiereinterfacegraphique.java
@@ -13,8 +13,6 @@ public class Mapremiereinterfacegraphique {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+    
     
 }
