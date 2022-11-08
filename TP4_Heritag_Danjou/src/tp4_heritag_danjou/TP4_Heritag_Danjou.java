d24e3e3afb5a708fe60ad108f41ce404862a5c24
matthieu danjou <matthieu.danjou@epfedu.fr>
2022-11-08 16:32:44 +0100

    avancée tp4 jusqu'a la question 12

--- TP4_Heritag_Danjou/src/tp4_heritag_danjou/TP4_Heritag_Danjou.java
+++ TP4_Heritag_Danjou/src/tp4_heritag_danjou/TP4_Heritag_Danjou.java
@@ -4,6 +4,8 @@
  */
 package tp4_heritag_danjou;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Utilisateur
@@ -19,9 +21,25 @@ public class TP4_Heritag_Danjou {
     Epée Epée1 = new Epée("Excalibur",7,5); 
         System.out.println(Epée1) ;    
         
-    Epée Epée2 = new
+    Epée Epée2 = new Epée("Durandal",4,7);
+        System.out.println(Epée2);
+    
+    Baton Baton1 =new Baton("Chêne", 4, 5);
+    System.out.println(Baton1);
+    
+    Baton Baton2 =new Baton("Charme", 5, 6);
+    System.out.println(Baton2);
         
-        
-    }
+    
+    ArrayList <Arme> nom_Arme= new ArrayList <Arme>();
+    nom_Arme.add(Epée1);
+    nom_Arme.add(Epée2);
+    nom_Arme.add(Baton1);
+    nom_Arme.add(Baton2);
+    
+    
+    
+    
+    
     
 }
